9832dc53af0204a2e3aa162250f23e636a0163c8
HirushiMandakini <hirushimandakinimendis@gmail.com>
2025-02-24 07:41:01 +0530

    completed order detail crud

--- src/database/orderdetail.ts
+++ src/database/orderdetail.ts
@@ -24,66 +24,66 @@ export async function saveOrderDetail(orderDetail: {orderId: string, flowerBouqu
     }
    
 }
-// // update orderDetail
-// export async function updateOrderDetail(id: string, updated: {
-//     orderId: string,
-//     flowerbouquetId: string,
-//     quantity: number,
-//     price: number
-// }){
-//     try{
-//         const updatedOrderDetail = await prisma.orderItem.update({
-//             where: {id:id},
-//             data: updated
-//         });
-//         console.log("Order detail updated successfully:", updatedOrderDetail);
-//         return updatedOrderDetail;
-//     }
-//     catch(error){
-//         console.error("Error updating order detail:", error);
-//         throw error;
-//     }
-// }
+ // update orderDetail
+    export async function updateOrderDetail(id: string, updated: {
+     orderId: string,
+     flowerBouquetId: string,
+     quantity: number,
+     price: Decimal
+ }){
+     try{
+         const updatedOrderDetail = await prisma.orderItem.update({
+             where: {id:id},
+             data: updated
+         });
+         console.log("Order detail updated successfully:", updatedOrderDetail);
+         return updatedOrderDetail;
+     }
+     catch(error){
+         console.error("Error updating order detail:", error);
+         throw error;
+     }
+ }
 
-// // delete orderDetail
-// export async function deleteOrderDetail(id: string) {
-//     try{
-//         const deletedOrderDetail = await prisma.orderItem.delete({
-//             where: {id:id},
-//         });
-//         console.log("Order detail deleted successfully:", deletedOrderDetail);
-//         return deletedOrderDetail;
-//     }
-//     catch(error){
-//         console.error("Error deleting order detail:", error);
-//         throw error;
-//     }
-// }
+ // delete orderDetail
+ export async function deleteOrderDetail(id: string) {
+     try{
+         const deletedOrderDetail = await prisma.orderItem.delete({
+             where: {id:id},
+         });
+         console.log("Order detail deleted successfully:", deletedOrderDetail);
+         return deletedOrderDetail;
+     }
+     catch(error){
+         console.error("Error deleting order detail:", error);
+         throw error;
+     }
+ }
 
-// // get all orderDetails
-// export async function getAllOrderDetails() {
-//     try{
-//         const allOrderDetails = await prisma.orderItem.findMany();
-//         return allOrderDetails;
-//     }
-//     catch(error){
-//         console.error("Error getting all order details:", error);
-//         throw error;
-//     }
-// }
+ // get all orderDetails
+ export async function getAllOrderDetails() {
+     try{
+         const allOrderDetails = await prisma.orderItem.findMany();
+         return allOrderDetails;
+     }
+     catch(error){
+         console.error("Error getting all order details:", error);
+         throw error;
+     }
+ }
 
 // // get orderDetail by id
-// export async function getOrderDetailById(id: string) {
-//     try{
-//         const orderDetail = await prisma.orderItem.findUnique({
-//             where: {id:id},
-//         });
-//         return orderDetail;
-//     }
-//     catch(error){
-//         console.error("Error getting order detail by id:", error);
-//         throw error;
-//     }
-// }
+ export async function getOrderDetailById(id: string) {
+     try{
+         const orderDetail = await prisma.orderItem.findUnique({
+             where: {id:id},
+         });
+         return orderDetail;
+     }
+     catch(error){
+         console.error("Error getting order detail by id:", error);
+         throw error;
+     }
+ }
 
 
--- src/route/OrderdetailRoute.ts
+++ src/route/OrderdetailRoute.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import OrderDetail from '../model/orderdetailModel';
-import { saveOrderDetail} from '../database/orderdetail';
+import { saveOrderDetail, updateOrderDetail, deleteOrderDetail, getOrderDetailById, getAllOrderDetails} from '../database/orderdetail';
 import { PrismaClient } from '../../node_modules/.prisma/client/index';
 
 const router = express.Router();
@@ -16,5 +16,53 @@ router.post('/add',async (req,res)=>{
         res.send("Error Occured");
     }
 })
-export default router;
 
+
+// update 
+router.put('/update/:id',async (req, res) => {
+    const id = req.params.id;   
+    const OrderDetail:OrderDetail=req.body;
+    try{
+        const orderDetailUpdate = await updateOrderDetail(id,OrderDetail);
+        res.send("Order detail updated successfully");
+        }
+        catch(error){
+        res.send("Error in updating order detail");
+        }
+});
+
+// delete
+router.delete('/delete/:id',async (req, res) => {
+    const id = req.params.id;
+    try{
+        const orderDetailDelete = await deleteOrderDetail(id);
+        res.send("Order detail deleted successfully");
+        }
+        catch(error){
+            res.send("Error in deleting order detail");
+            }
+            });
+
+// search
+router.get('/search/:id',async (req,res)=>{
+    const id = req.params.id;
+    try{
+        const orderDetailSearch=await getOrderDetailById(id);
+        res.send(orderDetailSearch);
+    }
+    catch(error){
+        res.send("Error Occured");
+    }
+});
+
+// getall
+router.get('/all',async (req,res)=>{
+    try{
+        const orderDetailAll = await getAllOrderDetails();
+        res.send(orderDetailAll);
+    }
+    catch(error){
+        console.log(error)
+    }
+})
+export default router;
